cb2dbc417c201b60cd2ae1a873f2850465cf2ad5
Grigory Morozov <noellhill@yandex.ru>
2025-05-10 20:50:26 +0300

    использовал модули для параллельного запуска процессов: текста и музыки, также плавное появление текста

--- game_HERO/game_text-1/game-1.py
+++ game_HERO/game_text-1/game-1.py
@@ -1,20 +1,39 @@
 import time
+from playsound import playsound
+import threading
+
+def pd(text, delay = 0.05):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+    
+def play_music(music_file, stop_flag):
+    while not stop_flag.is_set():
+        playsound(music_file)
+        
+def main():
+    stop_flag = threading.Event()
+    music_thread = threading.Thread(target=play_music, args=('01 - Opening.ogg', stop_flag))
+    music_thread.start()
+    pd('''РЫЦАРЬ, КОТОРЫЙ НИЧЕГО НЕ БОИТСЯ''')
+    stop_flag.set()
+    music_thread.join()
+    stop_flag.clear() 
+    
+    music_thread = threading.Thread(target=play_music, args=('09 - Z 339 - Here the Deities approve.ogg', stop_flag))
+    music_thread.start()
+    pd('''В дни, когда земля ещё хранила эхо древних битв,
+когда магия струилась в жилах мира,
+а герои были не просто именами в легендах,
+жил-был рыцарь, чьё сердце пылало отвагой и честью.
+Он стоял на холме, где ветер шептал тайны веков,
+а солнце бросало лучи на его сверкающие доспехи.''')
+    stop_flag.set()
+    music_thread.join()
+    
+    
 
 
-class Hero:
-    def __init__(self, name, health=100, mana=100, level=1):
-        self.name = name
-        self.health = health
-        self.mana = mana
-        self.level = level
-
-    def attack(self, damage):
-        self.health -= damage
-
-
-startGame = True
-
-while startGame:
-    print('Добро пожаловать в игру')
-    time.sleep(2)
-
+if __name__ == "__main__":
+    main()
