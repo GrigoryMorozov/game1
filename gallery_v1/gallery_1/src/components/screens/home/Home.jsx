c7137deca8c03cc8befdbc2d0ce00a27378d69df
Grigory Morozov <noellhill@yandex.ru>
2025-05-18 23:22:48 +0300

    Вступление. Но!!! музыка, надо подумать, как сделать, чтобы она останавливалась там где нужно

--- gallery_v1/gallery_1/src/components/screens/home/Home.jsx
+++ gallery_v1/gallery_1/src/components/screens/home/Home.jsx
@@ -7,18 +7,14 @@ function Home() {
     return (
       <div>
         <h1>
-          Catalog car
+          Linux Catalog
         </h1>
         <div>
             <div className={styles.item}>
-                <div className={styles.image} style={
-                    {
-                        backgroundImage:'url(/1.jpg)',
-                    }
-                }/>
+                <div className={styles.image}/>
                 <div className={styles.info}>
 
-                    <h2>Car1</h2>
+                    <h2>Arch</h2>
                     <p>$100 000</p>
                     <button>Read more</button>
                 </div>
--- gallery_v1/gallery_1/src/main.jsx
+++ gallery_v1/gallery_1/src/main.jsx
@@ -2,10 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './components/screens/home/Home'
 import './assets/styles/global.css'
+// import styles from './main.module.css'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    
+      <Home />
+     
+    
+    
   </StrictMode>,
 )
